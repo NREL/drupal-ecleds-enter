b1d0dbd26573b4c49ef17542bdebf30e6a39a0b3
Haley <damon.haley@nrel.gov>
2015-07-01 13:19:33 -0600

    ***** Add and allow editing and viewing by all users - New fields - create three brand new field ****** FY15 Results - field_fy15_results ****** FY18 Target - field_fy18_target ****** FY15 Results Explanation - field_fy15_results_explanation

--- sites/all/themes/ecleds/templates/page/maintenance-page--offline.tpl.php
+++ sites/all/themes/ecleds/templates/page/maintenance-page--offline.tpl.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
