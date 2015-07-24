3fd1aa23ad0cef289a0c4ae4e4de448deeb10107
Shaun Laws <Shaun.Laws@nrel.gov>
2015-07-24 16:18:23 -0600

    fix header area

--- sites/all/themes/ecleds/templates/page/page.tpl.php
+++ sites/all/themes/ecleds/templates/page/page.tpl.php
@@ -11,18 +11,18 @@ global $user;
 
 // An anonymous user has a user id of zero.
 if ($user->uid > 0) : ?>
-<div class=top-fixed-area">
-  <div id="fixed-branding">
+<div class="top-fixed-area">
+  <div id="fixed-branding"<?php print (empty($primary_local_tasks['#primary'])) ? ' class="no-primary"' : ''; ?>>
 
     <?php print $breadcrumb; ?>
 
     <?php print render($title_prefix); ?>
 
-    <?php if ($title): ?>
-      <h1 class="page-title"><?php print $title; ?></h1>
-    <?php endif; ?>
+      <?php if ($title): ?>
+        <h1 class="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
 
-    <?php print render($title_suffix); ?>
+      <?php print render($title_suffix); ?>
 
     <?php if ($page['header']): ?>
       <div id="header">
