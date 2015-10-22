f3c6626f0293cb10fe255cc5cfd5feb8f389c926
Haley <damon.haley@nrel.gov>
2015-10-22 15:32:08 -0600

    settings local

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -74,10 +74,20 @@ if (isset($_SERVER['WWW_NREL']) || PHP_SAPI === 'cli') {
 // Set the maintenance theme.
 $conf['maintenance_theme'] = 'ecleds';
 
+?>
+
+<?php
 /**
- * Include a local settings file if it exists.
+ * Load local development override configuration, if available.
+ *
+ * Use settings.local.php to override variables on secondary (staging,
+ * development, etc) installations of this site. Typically used to disable
+ * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
+ * other things that should not happen on development and testing sites.
+ *
+ * Keep this code block at the end of this file to take full effect.
  */
-$local_settings = dirname(__FILE__) . '/settings.local.php';
-if (file_exists($local_settings)) {
-  include $local_settings;
+if (file_exists(__DIR__ . '/settings.local.php')) {
+ include __DIR__ . '/settings.local.php';
 }
+?>
